c719be505b097eb36b349d0249b3694e6ef2aaa4
HerberthReyes07 <herberthreyes202230236@cunoc.edu.gt>
2024-02-21 21:02:48 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/ipc2/ptc1/git/Ipc2Ptc1Git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ipc2.ptc1.git;
+
+/**
+ *
+ * @author herberthreyes
+ */
+public class Ipc2Ptc1Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
